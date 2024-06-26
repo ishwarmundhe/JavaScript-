08884d9f2d345a8472076ac81cd2d2814e3ce6f5
Ishwar Mundhe <ishwarmundhe.dev@gmail.com>
2024-06-26 11:36:37 +0000

    const, let , var

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123234
+let accountName = "Ishwar Mundhe "
+var accountCity = "Pune"
+accountCollege = "SCOE"
+let accountState;
+
+console.table([accountId, accountName, accountCity, accountCollege ,accountState])
