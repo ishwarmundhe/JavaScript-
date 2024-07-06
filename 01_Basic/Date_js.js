5a729b8799289c294bd721a8feb35461df920068
Ishwar Mundhe <ishwarmundhe.dev@gmail.com>
2024-07-06 05:24:54 +0000

    Date in JS

---
+++ 01_Basic/Date_js.js
@@ -0,0 +1,18 @@
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+
+//let myCreatedDate = new Date(2023, 0, 23)
+//let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("2023-01-14")
+
+console.log(myCreatedDate.toString());
+
+let Date = 
+
