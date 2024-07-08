92b08f8c0ec1918a89c36f60f340c49a2cc23c5d
Ishwar Mundhe <ishwarmundhe.dev@gmail.com>
2024-07-08 08:50:46 +0000

    Err

--- 02_Basic/02_arrays.js
+++ 02_Basic/02_arrays.js
@@ -17,7 +17,7 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
-
+ 
 
 
 console.log(Array.isArray("Hitesh"))
