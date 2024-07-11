65fe775ef77142aa40eac1a0a09a7e41015a72e3
Ishwar Mundhe <ishwarmundhe.dev@gmail.com>
2024-07-11 12:05:50 +0000

    IFFE

---
+++ 04_Control_flow/02_switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+// break; not there all below code run until default 
