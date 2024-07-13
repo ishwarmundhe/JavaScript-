d2b08673e9e0f41cb0d32a3248980dfa5e26b603
Ishwar Mundhe <ishwarmundhe.dev@gmail.com>
2024-07-13 07:00:16 +0000

    while-do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,42 @@
+// While and do while
+
+let num = 1;
+while (num <= 10) {
+    // console.log(`value of num is ${num}`);
+    num++;
+//     num = num + 2;
+}
+
+// let num2 = 11;
+// while (num2 <= 10) {
+//     console.log(`value of num is ${num}`);
+//     num2++;
+// //     num = num + 2;
+// }
+
+// do while
+
+
+let value = 1;
+do {
+    // console.log(`value = ${value}`);
+    value++;
+} while (value <= 10);
+
+let value1 = 11;
+do {
+    console.log(`value = ${value1}`);
+    value1++;
+} while (value1 <= 10);
+
+
+// array while loop
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+    
