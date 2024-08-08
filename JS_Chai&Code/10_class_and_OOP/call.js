eaf752ada392575895da55a069679b58f8ecb5f7
Ishwar Mundhe <ishwarmundhe007@gmail.com>
2024-08-08 13:16:15 +0530

    call method

--- JS_Chai&Code/10_class_and_OOP/call.js
+++ JS_Chai&Code/10_class_and_OOP/call.js
@@ -2,10 +2,14 @@ function setUsername(username) {
     this.username = username
 }
 function createUsername(username, email, password) {
-    setUsername(username)
+    setUsername.call(this,username)
     this.email = email
     this.password = password
     
 }
 const user = new createUsername("Ishwar", "ishu@gmail.com", "1234")
 console.log(user);
+
+
+// call method is used to acces setUSername from beyound scope as when the scope setUsername call calltrack will delete it ref when use 
+// as it need to be stay to acces content in setUsername form createUsername __.call( )-method is call
